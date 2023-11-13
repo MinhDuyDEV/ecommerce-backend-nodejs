255918cb632de07241bb5b14492658ad0e2208e6
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-11-13 17:56:55 +0700

    connect to Mongodb & config dbs

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
 // init db
+require("./database/init.mongodb");
 
 // init routes
 
---
+++ src/database/init.mongodb.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const { default: mongoose } = require("mongoose");
+const { countConnect } = require("../helpers/check.connect");
+
+const connectString = `mongodb://localhost:27017/shopDev`;
+
+class Database {
+  constructor() {
+    this.connect();
+  }
+  connect() {
+    if (1 === 1) {
+      mongoose.set("debug", true);
+      mongoose.set("debug", { color: true });
+    }
+    mongoose
+      .connect(connectString, { maxPoolSize: 50 })
+      .then((_) => {
+        console.log("Connected Mongodb Success");
+        countConnect();
+      })
+      .catch((error) => console.log("Error Connect!!!"));
+  }
+  static getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+    return Database.instance;
+  }
+}
+
+const instanceMongodb = Database.getInstance();
+module.exports = instanceMongodb;
