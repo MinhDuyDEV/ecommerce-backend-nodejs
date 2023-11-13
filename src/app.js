3c42c99123aceb7a3c5ba2422958c5bbefb3a8fa
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-11-13 17:15:47 +0700

    Initial project

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
+const compression = require("compression");
+
+const app = express();
+
+// init middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+// init db
+
+// init routes
+
+// handling error
+
+module.exports = app;
