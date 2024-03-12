860f626158cd8836b53193c0c520a8444227612a
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2024-03-13 04:39:49 +0700

    fix: change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 2201;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`App start with port: ${PORT}`);
